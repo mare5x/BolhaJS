308a135d2af52ab48e7c8f7011b443b0a3f1a209
mare5x <mare5x.dev@gmail.com>
2019-08-22 17:01:01 +0200

    Fetch multiple pages.

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
+/** 
+ * Watch for changes to multiple filtered queries of articles.
+ * Parse the results and generate a HTML report. 
+*/
+
 const requests = require('request-promise-native');
 const cheerio = require('cheerio');
 
 
-const BOLHA_URL = 'http://www.bolha.com/';
+const BOLHA_URL = 'http://www.bolha.com';
+
 
 let fetch = async function (url) {
     let options = {
@@ -14,14 +20,24 @@ let fetch = async function (url) {
     return await requests.get(options);
 };
 
-let get_articles = async function (query) {
-    const query_fmt = `iskanje?q=${query}&sort=1`;
-    let url = BOLHA_URL + query_fmt;
-    let html = await fetch(url);
-    console.log(`Got ${url} -> ${html.length} bytes`);
+let fetch_page = async function (url, page) {
+    let full_url = url + `&page=${page}`;
+    let html = await fetch(full_url);
+    return cheerio.load(html);
+};
 
+let build_query_url = function (query, sort = 1) {
+    const query_fmt = `/iskanje?q=${query}&sort=${sort}`;
+    return BOLHA_URL + query_fmt;
+}
+
+let get_page_count = function ($) {
+    let el = $('div.pager a.last');
+    return el.length > 0 ? parseInt(el.first().text()) : 1;
+};
+
+let get_articles_on_page = function ($) {
     let articles = [];
-    const $ = cheerio.load(html);
     $('section#list div.ad').each((_, element) => {        
         let el = $(element).find($('div.price')).children().last();
         if (el.length == 0) return;  // skip articles without a price (ads ...)
@@ -33,11 +49,38 @@ let get_articles = async function (query) {
         articles.push(data);
     });
     return articles;
+};
+
+let get_articles = async function (query, pages=-1) {
+    // If `pages` is -1, get all pages otherwise, up to `pages`
+    let url = build_query_url(query);
+
+    console.log('Fetching page 1 ...');
+    const $ = await fetch_page(url, 1);
+    let page_count = get_page_count($);
+    let page_end = pages <= 0 ? page_count : Math.min(page_count, pages)
+
+    let articles = get_articles_on_page($);
+
+    async function fetch_and_parse_page(page) {
+        console.log(`Fetching page ${page} ...`);
+        let page$ = await fetch_page(url, page);
+        for (let article of get_articles_on_page(page$)) {
+            articles.push(article);
+        }
+    }
+    let promises = [];
+    for (let page = 2; page <= page_end; ++page) {
+        promises.push(fetch_and_parse_page(page));
+    }
+    await Promise.all(promises);
+
+    return articles;
 }
 
 let main = async function () {
-    let articles = await get_articles('lenovo');
-    console.log(articles);
+    let articles = await get_articles('lenovo', 1);
+    console.log(`Found ${articles.length} articles`);
 };
 
 
